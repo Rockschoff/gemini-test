0918aa2ce6bef0682ac12ede784d31b63e3d899b
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2023-12-16 15:00:14 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import {data} from "./config.js";
+export default function App() {
+  const [output, setOutput] = useState("");
+  const [loading, setLoading] = useState("");
+  const api_key = data.API_KEY; // Enter your google API Key here
+  const genAI = new GoogleGenerativeAI(api_key);
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const prompt = "say something short and interesting";
 
-function App() {
+  async function callAPI() {
+    setLoading("Loading");
+    var p = document.getElementById("textfield").value;
+    if (p === "") {
+      p = prompt;
+    }
+
+    const result = await model.generateContent(p);
+    const response = await result.response;
+    const text = await response.text();
+    setLoading("");
+    setOutput(text);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello!</h1>
+      <h2>Type Something in the formfield to get a response</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          callAPI();
+        }}
+      >
+        <input
+          type="text"
+          id="textfield"
+          defaultValue="Tell me a dad joke"
+        ></input>
+        <input type="submit"></input>
+      </form>
+      <p>{loading}</p>
+      <p>{output}</p>
     </div>
   );
 }
-
-export default App;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const data = {
+    API_KEY: "AIzaSyBPujn54DHMmaY_Z79SQUO-iOjm1VCDTsc",
+  };
+  
